bfa8581d1e30903fc1ad9078d75ff996edf8cbbd
marco.cantu <marco.cantu@unosquare.com>
2022-07-15 15:45:02 -0500

    remove and fix api

--- Unosquare.CodeChallenge/WarehouseRESTfulAPI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WarehouseRESTfulAPI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Unosquare.CodeChallenge/WarehouseRESTfulAPI/Program.cs
+++ Unosquare.CodeChallenge/WarehouseRESTfulAPI/Program.cs
@@ -1,34 +1,17 @@
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// get the connection string 
-var connectionString = builder.Configuration.GetConnectionString("DevConnection");
-
 // Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<WarehouseCoreLib.DataAccess.WarehouseDbContext>(options => 
-    options.UseSqlServer(connectionString));
+
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllers();
 
 app.Run();
